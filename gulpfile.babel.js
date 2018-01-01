7e58d903a8f78097d86f76d886502ce01ae8c25f
sakuramodki <sakuramodki@gmail.com>
2018-01-02 08:42:37 +0900

    downgrade to 6.26.0 and add test scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,14 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 
 gulp.task('test', (callback) => {
   console.log('test done');
   callback();
 });
+
+gulp.task('default', (callback) => {
+  gulp.src('src/scripts/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./assets/'))
+});
+
